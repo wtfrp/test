695e887fee20d2b245ce1ce2d66fd0cf1224fbea
wtfrp <1935606909@qq.com>
2016-08-21 20:52:29 +0800

    asd

---
+++ helloworld.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+#define MAX_LEN 100
+int main()
+{
+	int i = MAX_LEN;
+
+	for(i = 0; i < 7; i++)
+	{
+		printf("HelloWorld!\n");
+	}
+	printf("123 \n");
+	printf("aaa modified\n");
+	printf("bbb modified\n");
+	printf("bbb modified\n");
+	printf("czw modified\n");
+	printf("zzz modifyed\n");
+	return 0;
+}
